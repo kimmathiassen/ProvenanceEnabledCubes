0e1aa93ef6545f8707281ad7d8451bc4c20ac08d
kimajakobsen <kimajakobsen@gmail.com>
2016-12-10 20:33:26 +0100

    now basic triple paterns are split in a proper way

--- src/main/java/dk/aau/cs/qweb/pec/QueryEvaluation/AnalyticalQuery.java
+++ src/main/java/dk/aau/cs/qweb/pec/QueryEvaluation/AnalyticalQuery.java
@@ -3,9 +3,9 @@ package dk.aau.cs.qweb.pec.QueryEvaluation;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.tuple.Pair;
@@ -19,55 +19,90 @@ public class AnalyticalQuery {
 	private String  lastPart = "";
 	private List<Signature> triplePatterns = new ArrayList<Signature>(); 
 	private List<String> fromClause = new ArrayList<String>();
+	private Map<String,String> prefixes = new HashMap<String,String>();
+
 
 	public AnalyticalQuery(File queryFile, RDFCubeStructure structure) throws IOException {
 		
 		originalQuery = FileUtils.readFileToString(queryFile);
-		firstPart = extractString("SELECT","WHERE");
-		lastPart = extractString("WHERE","\\Z");
+		String[] split = originalQuery.split("WHERE");
+		firstPart = split[0];
+		lastPart = split[1];
 		
-		for (String triplePattern : lastPart.split("\\.")) {
-			triplePattern = triplePattern.replaceAll("{", "");
-			triplePattern = triplePattern.replaceAll("}", "");
-			triplePattern = triplePattern.trim();
+		getPrefixes(firstPart);
+		
+		for (String triplePatternDotSplit : lastPart.split("\\.")) {
+			triplePatternDotSplit = triplePatternDotSplit.replaceAll("\\{", "");
+			triplePatternDotSplit = triplePatternDotSplit.replaceAll("\\}", "");
+			triplePatternDotSplit = triplePatternDotSplit.trim();
 			
-			if (triplePattern.contains("UNION")) { 				
-			} else if (triplePattern.contains("FILTER")) {				
+			if (triplePatternDotSplit.contains("UNION")) { 				
+			} else if (triplePatternDotSplit.contains("FILTER")) {				
 			} else {
-				String[] elements = triplePattern.split(" ");
+				String subject = "";
+				String predicate = "";
+				String object = "";
 				
-				if (elements.length != 3) {
-					throw new IllegalArgumentException("triples pattern could not be passed: "+ triplePattern);
+				for (String triplePatternSimiColonSplit : triplePatternDotSplit.split(";")) {
+					triplePatternSimiColonSplit = triplePatternSimiColonSplit.trim();
+					String[] elements = triplePatternSimiColonSplit.split(" ");
+					
+					if (elements.length == 3) {
+						//throw new IllegalArgumentException("triples pattern could not be passed: "+ triplePattern);
+						subject = addPrefix(elements[0]);
+						predicate = addPrefix(elements[1]);
+						object = addPrefix(elements[2]);
+					} else if (elements.length == 2) {
+						predicate = addPrefix(elements[0]);
+						object = addPrefix(elements[1]);
+					}
+					
+					Pair<String, String> domainRange = structure.getDomainAndRange(predicate);
+					
+					if (subject.contains("?")) {
+						subject = domainRange.getKey();
+					}
+					
+					if (object.contains("^^")) {
+						String[] hatSplit = object.split("^^");
+						object = hatSplit[1];
+					} else {
+						object = domainRange.getValue();
+					}
+					triplePatterns.add(new Signature(subject, predicate, object, null));
 				}
-				String subject = elements[0];
-				String predicate = elements[1];
-				String object = elements[2];
 				
-				Pair<String, String> domainRange = structure.getDomainAndRange(subject);
 				
-				if (subject.contains("?")) {
-					subject = domainRange.getKey();
-				}
-				
-				if (object.contains("^^")) {
-					String[] hatSplit = object.split("^^");
-					object = hatSplit[1];
-				} else {
-					object = domainRange.getValue();
-				}
-				triplePatterns.add(new Signature(subject, predicate, object, null));
 			}
 		}
 	}
 
-	private String extractString(String string, String string2) {
-		String result = "";
-		Pattern p = Pattern.compile(Pattern.quote(string) + "(.*?)" + Pattern.quote(string2));
-		Matcher m = p.matcher(originalQuery);
-		while (m.find()) {
-			result = m.group(1);
+	private void getPrefixes(String firstPart2) {
+		String[] selectSplit = firstPart2.split("SELECT");
+		String[] PrefixSplit = selectSplit[0].split("PREFIX");
+		for (String prefix : PrefixSplit) {
+			if (!prefix.isEmpty()) {
+				prefix = prefix.trim();
+				String[] split =  prefix.split(" ");
+				String url = split[1];
+				url = url.replaceAll("<", "");
+				url = url.replaceAll(">", "");
+				
+				prefixes.put(split[0], url);
+			}
 		}
-		return result;
+	}
+
+	private String addPrefix(String string) {
+		if (string.contains(":")) {
+			String[] colonSplit = string.split(":");
+			if (colonSplit[0].isEmpty()) {
+				return "<"+prefixes.get(":")+colonSplit[1]+">";
+			} else {
+				return "<"+prefixes.get(colonSplit[0])+colonSplit[1]+">";
+			}
+		}
+		return string;
 	}
 
 	public List<Signature> getTriplePatterns() {

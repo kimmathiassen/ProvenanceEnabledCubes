7cd56ae78f2286a87dc6502df8968d0cfe3daf52
kimajakobsen <kimajakobsen@gmail.com>
2016-12-12 10:28:30 +0100

    iterate over provenance query

--- src/main/java/dk/aau/cs/qweb/pec/Experiment.java
+++ src/main/java/dk/aau/cs/qweb/pec/Experiment.java
@@ -4,12 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
+import dk.aau.cs.qweb.pec.QueryEvaluation.AnalyticalQuery;
 import dk.aau.cs.qweb.pec.QueryEvaluation.JenaMaterializedFragment;
 import dk.aau.cs.qweb.pec.QueryEvaluation.MaterializedFragments;
-import dk.aau.cs.qweb.pec.QueryEvaluation.AnalyticalQuery;
 import dk.aau.cs.qweb.pec.data.InMemoryRDFCubeDataSource;
 import dk.aau.cs.qweb.pec.data.JenaTDBDatabaseConnection;
 import dk.aau.cs.qweb.pec.data.RDFCubeDataSource;
@@ -19,9 +18,6 @@ import dk.aau.cs.qweb.pec.exceptions.UnsupportedDatabaseTypeException;
 import dk.aau.cs.qweb.pec.fragment.Fragment;
 import dk.aau.cs.qweb.pec.fragmentsselector.FragmentsSelector;
 import dk.aau.cs.qweb.pec.fragmentsselector.GreedyFragmentsSelector;
-import dk.aau.cs.qweb.pec.fragmentsselector.ILPFragmentsSelector;
-import dk.aau.cs.qweb.pec.fragmentsselector.ImprovedILPFragmentsSelector;
-import dk.aau.cs.qweb.pec.fragmentsselector.NaiveFragmentsSelector;
 import dk.aau.cs.qweb.pec.lattice.Lattice;
 import dk.aau.cs.qweb.pec.lattice.NaiveLatticeBuilder;
 import dk.aau.cs.qweb.pec.types.Signature;
@@ -37,21 +33,21 @@ public class Experiment {
 	public Experiment() throws IOException, UnsupportedDatabaseTypeException, DatabaseConnectionIsNotOpen, GRBException, ParseException {
 		data = constructDataStore();
 		structure = RDFCubeStructure.build(Config.getCubeStructureLocation());
-		System.out.println(structure);
+		//System.out.println(structure);
 		NaiveLatticeBuilder builder = new NaiveLatticeBuilder();
 		lattice = builder.build(data, structure);
 		//System.out.println(lattice);
 		FragmentsSelector greedySelector = new GreedyFragmentsSelector(lattice, Config.getGreedyLogLocation());
-		FragmentsSelector ilpSelector = new ILPFragmentsSelector(lattice, Config.getILPLogLocation());
-		FragmentsSelector ilpImprovedSelector = new ImprovedILPFragmentsSelector(lattice, Config.getILPLogLocation());
-		FragmentsSelector naiveSelector = new NaiveFragmentsSelector(lattice, Config.getNaiveLogLocation());
+		//FragmentsSelector ilpSelector = new ILPFragmentsSelector(lattice, Config.getILPLogLocation());
+		//FragmentsSelector ilpImprovedSelector = new ImprovedILPFragmentsSelector(lattice, Config.getILPLogLocation());
+		//FragmentsSelector naiveSelector = new NaiveFragmentsSelector(lattice, Config.getNaiveLogLocation());
 		
-		materializedFragments = new JenaMaterializedFragment(ilpImprovedSelector.select(Config.getBudget()));
+		materializedFragments = new JenaMaterializedFragment(greedySelector.select(Config.getBudget()));
 		
-		System.out.println("[Greedy]: " + greedySelector.select(Config.getBudget()));
-		System.out.println("[ILP]: " + ilpSelector.select(Config.getBudget()));
-		System.out.println("[Improved ILP]: " + ilpImprovedSelector.select(Config.getBudget()));
-		System.out.println("[Naive]: " + naiveSelector.select(Config.getBudget()));		
+		//System.out.println("[Greedy]: " + greedySelector.select(Config.getBudget()));
+		//System.out.println("[ILP]: " + ilpSelector.select(Config.getBudget()));
+		//System.out.println("[Improved ILP]: " + ilpImprovedSelector.select(Config.getBudget()));
+		//System.out.println("[Naive]: " + naiveSelector.select(Config.getBudget()));		
 	}
 	
 	private RDFCubeDataSource constructDataStore() throws IOException, UnsupportedDatabaseTypeException {
@@ -68,45 +64,52 @@ public class Experiment {
 		Set<ProvenanceQuery> provenanceQueries = getProvenanceQueries();
 		ResultFactory resultFactory = new JenaResultFactory(Config.getResultLogLocation());
 		
-		List<String> provenanceIdentifiers =  resultFactory.evaluate(provenanceQueries); 
-		
-		for (AnalyticalQuery analyticalQuery : analyticalQueries) {
-			for (Signature partialTriplePatternSignature : analyticalQuery.getTriplePatterns()) {
-				Set<Fragment> allFragments = lattice.getFragmentsForRelation(partialTriplePatternSignature.getPredicate());
-				Set<Fragment> requiredFragments= removeFragmentsNotAllowedByProvenanceQuery(allFragments,provenanceIdentifiers);
-					
-				for (Fragment fragment : requiredFragments) {
-					
-					if(materializedFragments.contains(fragment)) {
-						analyticalQuery.addFrom(materializedFragments.getFragmentURL(fragment));
-					} else {
-						Set<Fragment> ancestors = lattice.getAncestors(fragment);
-						for (Fragment ancestor : ancestors) {
-							if (materializedFragments.contains(ancestor)) {
-								analyticalQuery.addFrom(materializedFragments.getFragmentURL(ancestor));
-							} else {
-								analyticalQuery.addFrom(fragment.getProvenanceIdentifers());
+		for (ProvenanceQuery provenanceQuery : provenanceQueries) {
+			System.out.println(provenanceQuery.getFilename());
+			Set<String> provenanceIdentifiers =  resultFactory.evaluate(provenanceQuery); 
+			System.out.println(resultFactory.evaluate(provenanceIdentifiers));
+			
+			for (AnalyticalQuery analyticalQuery : analyticalQueries) {
+				for (Signature partialTriplePatternSignature : analyticalQuery.getTriplePatterns()) {
+					Set<Fragment> allFragments = lattice.getFragmentsForRelation(partialTriplePatternSignature.getPredicate());
+					Set<Fragment> requiredFragments = removeFragmentsNotAllowedByProvenanceQuery(allFragments,provenanceIdentifiers);
+					System.out.println("Basic Triple Pattern "+partialTriplePatternSignature);
+					System.out.println(requiredFragments);
+						
+					for (Fragment fragment : requiredFragments) {
+						
+						if(materializedFragments.contains(fragment)) {
+							analyticalQuery.addFrom(materializedFragments.getFragmentURL(fragment));
+						} else {
+							Set<Fragment> ancestors = lattice.getAncestors(fragment);
+							for (Fragment ancestor : ancestors) {
+								if (materializedFragments.contains(ancestor)) {
+									analyticalQuery.addFrom(materializedFragments.getFragmentURL(ancestor));
+								} else {
+									analyticalQuery.addFrom(fragment.getProvenanceIdentifers());
+								}
 							}
 						}
 					}
 				}
+				resultFactory.evaluate(materializedFragments,analyticalQuery);
 			}
-			resultFactory.evaluate(materializedFragments,analyticalQuery);
 		}
+		
 	}
 
 	private Set<Fragment> removeFragmentsNotAllowedByProvenanceQuery(Set<Fragment> allFragments,
-			List<String> provenanceIdentifiers) {
+			Set<String> provenanceIdentifiers) {
 		Set<Fragment> allowedFragments = new HashSet<Fragment>();
 		for (Fragment fragment : allFragments) {
-			if (provenanceIdentifiers.contains(fragment.getProvenanceIdentifers())){
-				allFragments.add(fragment);
+			if (provenanceIdentifiers.containsAll(fragment.getProvenanceIdentifers())){
+				allowedFragments.add(fragment);
 			}
 		}
 		return allowedFragments;
 	}
 
-	private Set<ProvenanceQuery> getProvenanceQueries() {
+	private Set<ProvenanceQuery> getProvenanceQueries() throws IOException {
 		File folder = new File(Config.getProvenanceQueryPath());
 		Set<ProvenanceQuery> queries = new HashSet<ProvenanceQuery>();
 		for (File queryFile : folder.listFiles()) {

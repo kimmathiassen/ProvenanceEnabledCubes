4fd3632ee2788c93a6f9d4c0780e2be171f91022
kimajakobsen <kimajakobsen@gmail.com>
2017-01-03 10:31:25 +0100

    remove deprecated input parameters

--- src/main/java/dk/aau/cs/qweb/pec/Main.java
+++ src/main/java/dk/aau/cs/qweb/pec/Main.java
@@ -22,18 +22,8 @@ public class Main {
 		// create the Options
 		Options options = new Options();
 		options.addOption("h", "help", false, "Display this message." );
-		options.addOption("i", "load-instance-data", true, "path to media containing the instance data of the cube");
-		options.addOption("s", "load-cube-structure", true, "path to media containing the cube structure data");
-		options.addOption("p", "provenance-queries", true, "path to file containing provenance queries");
-		options.addOption("a", "analytical-queries", true, "path to file containing analytical queries");
-		options.addOption("t", "database-type", true, "choose between: in-memory, jena-tdb. Default: in-memory");
-		options.addOption("ilog", "ilp-log-location", true, "path to the file where the ILP-based fragment selector will log its messages");
-		options.addOption("glog", "greedy-log-location", true, "path to the file where the greedy fragment selector will log its messages");
-		options.addOption("nlog", "naive-log-location", true, "path to the file where the naive fragment selector will log its messages");
 		options.addOption("c", "config", true, "path to config file, this file takes precedens over other input");
-		options.addOption("b", "budget", true, "budget in terms of number of triples that will be materialized");
-		options.addOption("rlog", "result-log-location", true, "log file with exhaustive information about the run");
-		options.addOption("xlog", "experimental-log-location", true, "log file with the experimental information about the run");		
+		
 		try {
 		    CommandLine line = parser.parse( options, args );
 				    
@@ -41,50 +31,6 @@ public class Main {
 		    	printHelp(null,options);
 		    	System.exit(0);
 			} 
-			    
-		    if (line.hasOption("load-instance-data")) {
-				Config.addInstanceDataLocation(line.getOptionValue("load-instance-data"));
-			}
-				    
-		    if (line.hasOption("load-cube-structure")) {
-				Config.setCubeStructureLocation(line.getOptionValue("load-cube-structure"));
-			}
-		    
-		    if (line.hasOption("provenance-queries")) {
-				Config.addProvenanceQueryPath((line.getOptionValue("provenance-queries")));
-			}
-		    
-		    if (line.hasOption("analytical-queries")) {
-				Config.addAnalyticalQueryPath(addFolderOrFile(line.getOptionValue("analytical-queries")));
-			}
-		    
-		    if (line.hasOption("database-type")) {
-				Config.setDatabaseType(line.getOptionValue("database-type"));
-			}
-		    
-		    if (line.hasOption("ilp-log-location")) {
-		    	Config.setILPLogLocation(line.getOptionValue("ilp-log-location"));
-		    }
-		    
-		    if (line.hasOption("greedy-log-location")) {
-		    	Config.setGreedyLogLocation(line.getOptionValue("greedy-log-location"));
-		    }
-		    
-		    if (line.hasOption("naive-log-location")) {
-		    	Config.setGreedyLogLocation(line.getOptionValue("naive-log-location"));
-		    }
-		    
-		    if (line.hasOption("budget")) {
-		    	Config.addBudget(Long.parseLong(line.getOptionValue("budget")));
-		    }
-		    
-		    if (line.hasOption("result-log")) {
-		    	Config.setResultLogLocation(line.getOptionValue("result-log"));
-		    }
-		    
-		    if (line.hasOption("experimental-log")) {
-		    	Config.setExperimentalLogLocation(line.getOptionValue("experimental-log"));
-		    }
 				    
 		    if (line.hasOption("config")) {
 		    	try (BufferedReader br = new BufferedReader(new FileReader(line.getOptionValue("config")))) {

ebdb14f420ce11be13cbd56a3ccd38ed80c321e1
kimajakobsen <kimajakobsen@gmail.com>
2017-02-16 09:39:09 +0100

    change the lattice builder

--- src/main/java/dk/aau/cs/qweb/pec/lattice/LatticeBuilder.java
+++ src/main/java/dk/aau/cs/qweb/pec/lattice/LatticeBuilder.java
@@ -3,15 +3,41 @@ package dk.aau.cs.qweb.pec.lattice;
 import dk.aau.cs.qweb.pec.data.RDFCubeDataSource;
 import dk.aau.cs.qweb.pec.data.RDFCubeStructure;
 import dk.aau.cs.qweb.pec.exceptions.DatabaseConnectionIsNotOpen;
+import dk.aau.cs.qweb.pec.fragment.Fragment;
 
-public interface LatticeBuilder {
 
-	/**
-	 * Builds a fragments lattice from a given cube and its corresponding structure.
-	 * @param source
-	 * @param structure
-	 * @return
-	 * @throws DatabaseConnectionIsNotOpen 
-	 */
-	public Lattice build(RDFCubeDataSource source, RDFCubeStructure structure) throws DatabaseConnectionIsNotOpen;
+public class LatticeBuilder {
+
+	static public Lattice build(RDFCubeDataSource dataSource, RDFCubeStructure schema,String latticeMergeStrategy) throws DatabaseConnectionIsNotOpen {
+		Fragment root = Lattice.createFragment(); 
+		Lattice lattice;
+		
+		switch (latticeMergeStrategy) {
+		case "noMerge":
+			lattice = new NoMergeLattice(root, schema, dataSource);
+			break;
+		default:
+			lattice = new NoMergeLattice(root, schema, dataSource);
+			break;
+		}
+		
+		
+		try {
+			dataSource.open();
+			
+			while (dataSource.hasNext()) {
+				lattice.registerTuple(dataSource.next());
+			}
+			
+		} finally {
+			dataSource.close();
+		}
+		
+		// If a parent and a child have the same number of triples, 
+		// mark one of them as redundant
+		lattice.markRedundantFragments();
+			
+		return lattice;
+	}
+
 }
